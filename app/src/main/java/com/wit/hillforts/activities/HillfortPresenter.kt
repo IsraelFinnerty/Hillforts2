9e676f8f22f12118fbac47a9f988663cc96bcd88
Israel Finnerty <israelfinnerrty@gmail.com>
2021-01-06 12:37:19 +0000

    Moved Hillfort Activity to MVP

---
+++ app/src/main/java/com/wit/hillforts/activities/HillfortPresenter.kt
@@ -0,0 +1,160 @@
+package com.wit.hillforts.activities
+
+import android.content.Intent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.wit.hillforts.R
+import com.wit.hillforts.helpers.readImage
+import com.wit.hillforts.helpers.showImagePicker
+import com.wit.hillforts.main.MainApp
+import com.wit.hillforts.models.HillfortModel
+import com.wit.hillforts.models.Location
+import com.wit.hillforts.models.User
+import kotlinx.android.synthetic.main.activity_hillfort.*
+import org.jetbrains.anko.info
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.toast
+
+
+class HillfortPresenter(val view: HillfortView) {
+    var hillfort = HillfortModel()
+    var user = User()
+    lateinit var app: MainApp
+    val IMAGE_REQUEST1 = 1
+    val IMAGE_REQUEST2 = 2
+    val IMAGE_REQUEST3 = 3
+    val IMAGE_REQUEST4 = 4
+    val LOCATION_REQUEST = 5
+    lateinit var drawerLayout: DrawerLayout
+    var edit = false
+
+
+    init {
+        app = view.application as MainApp
+        if (view.intent.hasExtra("hillfort_edit")) {
+            edit = true
+            hillfort = view.intent.extras?.getParcelable<HillfortModel>("hillfort_edit")!!
+            view.showHillfort(hillfort)
+        }
+
+        if (view.intent.hasExtra("User"))
+        {
+            user = view.intent.extras?.getParcelable<User>("User")!!
+        }
+    }
+
+    fun doAddOrSave(
+        name: String,
+        description: String,
+        notes: String,
+        year: Int,
+        month: Int,
+        dayOfMonth: Int
+    ) {
+        hillfort.name = name
+        hillfort.description = description
+        hillfort.notes = notes
+        hillfort.dateVisitedYear = year
+        hillfort.dateVisitedMonth = month
+        hillfort.dateVisitedDay = dayOfMonth
+        if (hillfort.name.isNotEmpty()) {
+            if (edit) {
+                app.users.update(hillfort.copy(), user)
+            } else app.users.create(hillfort.copy(), user)
+            view.info("add Button Pressed: ${hillfort}")
+            view.setResult(AppCompatActivity.RESULT_OK)
+            view.startActivityForResult(
+                view.intentFor<HillfortListActivity>().putExtra("User", user), 0
+            )
+        } else {
+            view.toast(view.getString(R.string.enter_title))
+        }
+
+    }
+
+    fun doSelectImage1() {
+        showImagePicker(view, IMAGE_REQUEST1)
+    }
+
+    fun doSelectImage2() {
+        showImagePicker(view, IMAGE_REQUEST2)
+    }
+
+    fun doSelectImage3() {
+        showImagePicker(view, IMAGE_REQUEST3)
+    }
+
+    fun doSelectImage4() {
+        showImagePicker(view, IMAGE_REQUEST4)
+    }
+
+    fun doSetLocation() {
+        val location = Location(51.566804, -9.088011,  10f)
+        if (hillfort.lat != 0.0) {
+            location.lat =  hillfort.lat
+            location.lng = hillfort.lng
+            location.zoom = hillfort.zoom
+        }
+        view.startActivityForResult(view.intentFor<MapActivity>().putExtra("location", location), LOCATION_REQUEST)
+    }
+
+    fun doCheckChange(isChecked: Boolean) {
+        if (isChecked) {
+            view.date_visited.setVisibility(View.VISIBLE)
+
+            view.date_title.setVisibility(View.VISIBLE)
+            hillfort.visited = true
+        }
+        else {
+            view.date_visited.setVisibility(View.GONE)
+            view.date_title.setVisibility(View.GONE)
+            hillfort.visited = false
+        }
+    }
+
+
+    fun doActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        when (requestCode) {
+            IMAGE_REQUEST1 -> {
+                if (data != null) {
+                    hillfort.image1 = data.getData().toString()
+                    view.hillfortImage.setImageBitmap(readImage(view, resultCode, data))
+                    view.chooseImage.setText(R.string.button_changeImage)
+                }
+            }
+            IMAGE_REQUEST2 -> {
+                if (data != null) {
+                    hillfort.image2 = data.getData().toString()
+                    view.hillfortImage2.setImageBitmap(readImage(view, resultCode, data))
+                    view.chooseImage2.setText(R.string.button_changeImage2)
+                }
+            }
+            IMAGE_REQUEST3 -> {
+                if (data != null) {
+                    hillfort.image3 = data.getData().toString()
+                    view.hillfortImage3.setImageBitmap(readImage(view, resultCode, data))
+                    view.chooseImage3.setText(R.string.button_changeImage3)
+                }
+            }
+
+            IMAGE_REQUEST4 -> {
+                if (data != null) {
+                    hillfort.image4 = data.getData().toString()
+                    view.hillfortImage4.setImageBitmap(readImage(view, resultCode, data))
+                    view.chooseImage4.setText(R.string.button_changeImage4)
+                }
+            }
+
+            LOCATION_REQUEST -> {
+                if (data != null) {
+                    val location = data.extras?.getParcelable<Location>("location")!!
+                    hillfort.lat = location.lat
+                    hillfort.lng = location.lng
+                    hillfort.zoom = location.zoom
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/wit/hillforts/activities/HillfortView.kt
@@ -0,0 +1,178 @@
+package com.wit.hillforts.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.activity_hillfort.*
+import com.wit.hillforts.R
+import com.wit.hillforts.helpers.readImageFromPath
+import com.wit.hillforts.main.MainApp
+import com.wit.hillforts.models.HillfortModel
+import com.wit.hillforts.models.User
+import kotlinx.android.synthetic.main.activity_hillfort.description
+import kotlinx.android.synthetic.main.activity_hillfort.hillfortImage
+import kotlinx.android.synthetic.main.activity_hillfort_list.*
+import org.jetbrains.anko.*
+
+
+
+class HillfortView : AppCompatActivity(), AnkoLogger {
+
+    var hillfort = HillfortModel()
+    var user = User()
+    lateinit var app: MainApp
+    val IMAGE_REQUEST1 = 1
+    val IMAGE_REQUEST2 = 2
+    val IMAGE_REQUEST3 = 3
+    val IMAGE_REQUEST4 = 4
+    val LOCATION_REQUEST = 5
+    lateinit var drawerLayout: DrawerLayout
+
+    lateinit var presenter: HillfortPresenter
+    var placemark = HillfortModel()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort)
+        info("Hillfort Activity started..")
+
+        if (intent.hasExtra("User"))
+        {
+            user = intent.extras?.getParcelable<User>("User")!!
+        }
+
+        app = application as MainApp
+
+        drawerLayout = findViewById(R.id.drawer_layout_hillfort)
+
+        toolbarAdd.title = title
+        toolbarAdd.setNavigationIcon(R.drawable.ic_baseline_menu_24)
+        setSupportActionBar(toolbarAdd)
+
+        presenter = HillfortPresenter(this)
+        info("Hillfort Activity started..")
+
+
+        button_visited.setOnCheckedChangeListener { _, isChecked ->
+            presenter.doCheckChange(isChecked)
+                    }
+
+        btnAdd.setOnClickListener() {
+            presenter.doAddOrSave(hillfortName.text.toString(), description.text.toString(), notes.text.toString(), date_visited.year, date_visited.month, date_visited.dayOfMonth   )
+                    }
+
+        chooseImage.setOnClickListener { presenter.doSelectImage1() }
+        chooseImage2.setOnClickListener { presenter.doSelectImage2() }
+        chooseImage3.setOnClickListener { presenter.doSelectImage3() }
+        chooseImage4.setOnClickListener { presenter.doSelectImage4() }
+
+        hillfortLocation.setOnClickListener { presenter.doSetLocation() }
+
+
+        val check = drawerLayout.isDrawerOpen(GravityCompat.START)
+        toolbarAdd.setNavigationOnClickListener {
+            if (!check) drawerLayout.openDrawer(GravityCompat.START)
+            else  drawerLayout.closeDrawer(GravityCompat.START)
+        }
+
+        nav_view_hillfort.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_list -> {
+                    startActivityForResult(intentFor<HillfortListActivity>().putExtra("User", user), 0)
+                    true
+                }
+                R.id.nav_settings -> {
+                    startActivityForResult(intentFor<SettingsActivity>().putExtra("User", user), 0)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.nav_add -> {
+                    startActivityForResult(intentFor<HillfortView>().putExtra("User", user),0)
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                }
+                R.id.nav_logout -> {
+                    startActivity<LoginActivity>()
+                    true
+                }
+                else -> {
+                    if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+                        drawer_layout.closeDrawer(GravityCompat.START)
+                    }
+                    false
+                }
+            }
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (intent.hasExtra("hillfort_edit")) {
+            menuInflater.inflate(R.menu.menu_hillfort, menu)
+        }
+        else menuInflater.inflate(R.menu.menu_add, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.item_cancel -> finish()
+            R.id.item_delete -> {
+                app.users.delete(hillfort, user)
+                startActivityForResult(intentFor<HillfortListActivity>().putExtra("User", user), 0)
+            }
+            R.id.item_logout -> startActivity<LoginActivity>()
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data != null) {
+            presenter.doActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    fun showHillfort(hillfort: HillfortModel){
+        hillfortName.setText(hillfort.name)
+        description.setText(hillfort.description)
+        notes.setText(hillfort.notes)
+        button_visited.setChecked(hillfort.visited)
+        date_visited.updateDate(hillfort.dateVisitedYear, hillfort.dateVisitedMonth, hillfort.dateVisitedDay)
+        btnAdd.setText(R.string.button_editHillfort)
+
+        if (hillfort.visited) {
+            date_visited.setVisibility(View.VISIBLE)
+            date_title.setVisibility(View.VISIBLE)
+        }
+
+        if (hillfort.image1 != "") { chooseImage.setText(R.string.button_changeImage)
+            if (hillfort.image1.length > 20) { hillfortImage.setImageBitmap(readImageFromPath(this, hillfort.image1))
+            } else hillfortImage.setImageResource(this.getResources().getIdentifier(hillfort.image1, "drawable", this.packageName))
+        }
+
+        if (hillfort.image2 != "") { chooseImage2.setText(R.string.button_changeImage2)
+            if (hillfort.image2.length > 20) { hillfortImage2.setImageBitmap(readImageFromPath(this, hillfort.image2))
+            } else hillfortImage2.setImageResource(this.getResources().getIdentifier(hillfort.image2, "drawable", this.packageName))
+        }
+
+        if (hillfort.image3 != "") { chooseImage3.setText(R.string.button_changeImage3)
+            if (hillfort.image3.length > 20) { hillfortImage3.setImageBitmap(readImageFromPath(this, hillfort.image3))
+            } else hillfortImage4.setImageResource(this.getResources().getIdentifier(hillfort.image3, "drawable", this.packageName))
+        }
+
+        if (hillfort.image4 != "") { chooseImage4.setText(R.string.button_changeImage4)
+            if (hillfort.image4.length > 20) { hillfortImage4.setImageBitmap(readImageFromPath(this, hillfort.image4))
+            } else hillfortImage4.setImageResource(this.getResources().getIdentifier(hillfort.image4, "drawable", this.packageName))
+        }
+    }
+
+}
+
